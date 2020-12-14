9682515ac7f0f10e1683ad70118960ea3a8702ca
Michael Hueschen <m@mhueschen.space>
2020-12-14 09:59:55 -0700

    get hint working as initial test case

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,6 @@
 module Main where
 
-import qualified MyLib (someFunc)
+import qualified MyLib (eval)
 
-main :: IO ()
 main = do
-  putStrLn "Hello, Haskell!"
-  MyLib.someFunc
+  putStrLn "hi"
--- src/MyLib.hs
+++ src/MyLib.hs
@@ -1,4 +1,27 @@
-module MyLib (someFunc) where
+{-# LANGUAGE LambdaCase, ScopedTypeVariables, TypeApplications #-}
+module MyLib (eval) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import Data.Typeable (Typeable)
+import qualified Language.Haskell.Interpreter as Hint
+
+-- |
+-- Interpret expressions into values:
+--
+-- >>> eval @[Int] "[1,2] ++ [3]"
+-- [1,2,3]
+--
+-- Send values from your compiled program to your interpreted program by
+-- interpreting a function:
+--
+-- >>> f <- eval @(Int -> [Int]) "\\x -> [1..x]"
+-- >>> f 5
+-- [1,2,3,4,5]
+eval :: forall t. Typeable t
+     => String -> IO t
+eval s = do
+  res <- Hint.runInterpreter $ do
+    Hint.setImports ["Prelude"]
+    Hint.interpret s (Hint.as :: t)
+  case res of
+    Left err -> error ("error: " <> show err)
+    Right val -> pure val
